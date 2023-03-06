020b622f540cc789ca7fdec60d3a4c88834ee3c9
Ian Nawrocki <rocotto@Ians-MacBook-Pro.local>
2023-03-06 17:34:28 -0500

    Initialize repository

---
+++ lib/rocotto_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rocotto_palindrome/version"
+
+module RocottoPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rocotto_palindrome/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RocottoPalindrome
+  VERSION = "0.1.0"
+end
